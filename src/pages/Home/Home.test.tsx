c9153d86ace686d9398f88104ad3e5042869fe48
diegue-exe <dieeegue2104@gmail.com>
2023-06-02 19:56:27 +0200

    feat: Add test cases to verify gifs rendering

--- src/pages/Home/Home.test.tsx
+++ src/pages/Home/Home.test.tsx
@@ -1,24 +1,11 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import Home from './Home'
-import { waitFor } from '@testing-library/react'
 
 test('gif shows in the document', async () => {
   render(<Home />)
-  const element = screen.getByAltText('bob esponja enfadado')
 
-  await waitFor(() => expect(element).toBeInTheDocument())
+  expect(await screen.findByAltText('gatito')).toBeInTheDocument()
+  expect(await screen.findByAltText('bob esponja enfadado')).toBeInTheDocument()
+  expect(await screen.findByAltText('stewie')).toBeInTheDocument()
 })
-
-//test: que se muestre un gif
-
-//arange
-//creamos un gif con texto alternativo anAlternativeText
-//mockeamos la llamada para que nos devuelva un array con este gif
-
-//act
-//renderizamos
-
-//assert
-// Vamos a buscar el gif por texto alternativo
-// Vamos a ver que sea visible
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,18 @@
 import React from 'react'
 import spongeBob from '../../assets/images/gifs/spongeBob.gif'
+import stewie from '../../assets/images/gifs/stewie.gif'
 
 function Home() {
-  return <img src={spongeBob} alt="bob esponja enfadado" />
+  return (
+    <div>
+      <img src={spongeBob} alt="bob esponja enfadado" />
+      <img src={stewie} alt="gatito" />
+      <img
+        src="https://media.tenor.com/oeMHiLW62hAAAAAC/pretty-please-kitty-softpaws.gif"
+        alt="stewie"
+      />
+    </div>
+  )
 }
 
 export default Home
