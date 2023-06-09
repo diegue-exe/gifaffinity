71e81e38ccabe90217c382c644ce0b2455ef7f8d
Itziar Uzqueda <itziar.uzqueda@movalsys.com>
2023-06-09 18:18:37 +0200

    style: add grid style to gifs

--- src/components/atoms/Gif/Gif.tsx
+++ src/components/atoms/Gif/Gif.tsx
@@ -1,10 +1,10 @@
 import React, { FC } from 'react'
-
+import './Gif.css'
 type Props = {
   route: string
   alt: string
 }
 
 export const Gif: FC<Props> = ({ route, alt }) => {
-  return <img src={route} alt={alt} />
+  return <img className="gif" src={route}   alt={alt} />
 }
